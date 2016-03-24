280b8555fe1becd75910ce5ed4376294f9155063
Rita <ren.yuxi@yahoo.com>
2016-03-23 21:26:47 -0700

    finish this project, using restful api, mongodb, express

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var express = require("express"),
-bodyParser  = require("body-parser"),
-mongoose    = require("mongoose"),
-app 		= express();
+var express 	= require("express"),
+methodOverride  = require("method-override"),
+bodyParser  	= require("body-parser"),
+mongoose    	= require("mongoose"),
+app 			= express();
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_employee_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
 
 //MONGOOSE/MODEL CONFIG
 var employeeSchema = new mongoose.Schema({
@@ -19,13 +22,77 @@ var employeeSchema = new mongoose.Schema({
 	cellPhone: Number,
 	SMS: Number,
 	Email: String,
-	Manager: String,
-	DirectReport: Array
+	Manager: String
 });
-var Employee = mongoose.model("Employee", employeeSchema);
-
 //RESTFUL ROUTES
-
+var Employee = mongoose.model("Employee", employeeSchema);
+app.get("/", function(req, res){
+	res.redirect("/employee");
+});
+app.get("/employee", function(req, res){
+	Employee.find({}, function(err, employees){
+		if (err) {
+			console.log("ERROR");
+		} else {
+			res.render("index", {employees: employees});
+		}
+	});
+	
+});
+//NEW ROUTE
+app.get("/employee/new", function(req, res){
+	res.render("new");
+});
+//CREATE ROUTE
+app.post("/employee", function(req, res){
+	Employee.create(req.body.employees, function(err, newEmployee){
+		if (err){
+			res.render("new");
+		} else {
+			res.redirect("/employee");
+		}
+	});
+});
+//SHOW ROUTE
+app.get("/employee/:id", function(req, res){
+	Employee.findById(req.params.id, function(err, foundEmployee){
+		if(err) {
+			res.redirect("/employee");
+		} else {
+			res.render("show",{employee:foundEmployee});
+		}
+	})
+});
+//EDIT ROUTE
+app.get("/employee/:id/edit", function(req, res){
+	Employee.findById(req.params.id, function(err, foundEmployee){
+		if (err) {
+			res.redirect("employee");
+		} else {
+			res.render("edit", {employee:foundEmployee});
+		}
+	});
+});
+//UPDATE ROUTE
+app.put("/employee/:id", function(req, res){
+	Employee.findByIdAndUpdate(req.params.id, req.body.employees, function(err, updateEmployee){
+		if (err) {
+			res.redirect("/employee");
+		} else{
+			res.redirect("/employee/" + req.params.id);
+		}
+	});
+});
+//DESTROY ROUTE
+app.delete("/employee/:id", function(req, res){
+	Employee.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/employee");
+		} else {
+			res.redirect("/employee");
+		}
+	})
+});
 app.listen(3000, function(){
 	console.log("START!!! ");
 });
